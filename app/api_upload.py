16fcb55dc6d4af88f49857120477cbfeacdce621
Ange-Constance <constance.nimuhire@gmail.com>
2025-11-27 15:55:31 +0200

    add docker api file

--- app/api_upload.py
+++ app/api_upload.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9310d9688a2b8dce9307f42564f2980aaf332abb3b10836c4070ea9a1a6ce3d
-size 3529
+oid sha256:5feefcf524ac36ddf7e0eba069af93109229c8a413a43707bc06f823b46e3367
+size 3518
