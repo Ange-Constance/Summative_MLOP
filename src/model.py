b99cedf6e9ed95b8f5c04b97357479d4b33fa03a
Ange-Constance <constance.nimuhire@gmail.com>
2025-11-28 14:01:53 +0200

    Add model and data

---
+++ src/model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b7a54e3449213e8e05987be7516509ab09490cae10beed28a78c511db92a56ca
+size 743
---
+++ src/preprocessing.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e3e35e8081cebc4273d24fad3562e113789ca3f6d4860abe4ee6394345ceb41
+size 1153
